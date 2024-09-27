3f1f75a7f60581b49fc296b6cb36015402eea4ff
Michael Nwankwo <mike.nwankwo@outlook.com>
2024-09-27 02:34:37 +0100

    Setting up Dashboard along with Header and Footer Modules

--- src/components/Atoms/FooterAtom/FooterAtom.tsx
+++ src/components/Atoms/FooterAtom/FooterAtom.tsx
@@ -1,11 +1,49 @@
+import HomeSVG from "@svgs/home.svg";
+import MessageSVG from "@svgs/message.svg"; 
+import WalletSVG from "@svgs/wallet.svg"; 
+import HistorySVG from "@svgs/history.svg";
+
 type Props = {
-  title: string;
+  onHomeClick?: () => void;
+  onMessageClick?: () => void;
+  onWalletClick?: () => void;
+  onHistoryClick?: () => void;
 };
 
 export const FooterAtom = (props: Props) => {
   return (
-    <div className="flex h-full items-center justify-center bg-[#FFC96F]">
-      <div>{props.title}</div>
+    <div className="flex h-full items-center justify-between bg-[#262626] p-4">
+      {/* Home Tab */}
+      <div
+        className="cursor-pointer"
+        onClick={props.onHomeClick}
+      >
+        <HomeSVG width={24} height={24} fill="#8C8C8C" />
+      </div>
+
+      {/* Message Tab */}
+      <div
+        className="cursor-pointer"
+        onClick={props.onMessageClick}
+      >
+        <MessageSVG width={24} height={24} fill="#8C8C8C" />
+      </div>
+
+      {/* Wallet Tab */}
+      <div
+        className="cursor-pointer"
+        onClick={props.onWalletClick}
+      >
+        <WalletSVG width={24} height={24} fill="#FFFFFF" />
+      </div>
+
+      {/* History Tab */}
+      <div
+        className="cursor-pointer"
+        onClick={props.onWalletClick}
+      >
+        <HistorySVG width={24} height={24} fill="w#8C8C8C" />
+      </div>
     </div>
   );
 };
